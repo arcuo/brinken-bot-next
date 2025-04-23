da4d10624ec3c830d83fa1299952fd2a613672ba
Benjamin Zachariae <benjamin.zachariae@gmail.com>
2025-04-23 14:26:52 +0200

    cron changes

--- src/app/api/cron/day/route.ts
+++ src/app/api/cron/day/route.ts
@@ -1,7 +1,9 @@
 import { handleDay } from "@/app/actions/cron";
+import { log } from "@/lib/log";
 import type { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
 	// Handle day cron
+	log("Cron: Handling day cron");
 	handleDay();
 }
